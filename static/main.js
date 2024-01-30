fd7f66468781c74743a0b747f50797cd80324c2a
Zedbem <98301704+Zedbem@users.noreply.github.com>
2024-01-30 23:12:55 +0300

    Add files via upload

---
+++ static/main.js
@@ -0,0 +1,34 @@
+const host = 'http://localhost:3000';
+
+async function setProjects(){
+    const projects = await (await fetch(host + '/projects')).json();
+    const projectsContainer = document.getElementById('projects_holder');
+    for(const project of projects){
+        const col = document.createElement('div');
+        col.className = 'col-lg-6';
+        projectsContainer.append(col);
+        const card = document.createElement('div');
+        card.className ='card mb-4 shadow';
+        col.append(card);
+
+        const img = document.createElement('img');
+        img.src = project.image;
+        img.className = 'card-img-top';
+        card.append(img);
+
+        const cardBody = document.createElement('div');
+        cardBody.className = 'card-body p-4';
+        card.append(cardBody);
+
+        const title = document.createElement('h5');
+        title.innerHTML = project.name;
+        cardBody.append(title);
+
+        const desc = document.createElement('span');
+        desc.className = 'card-text';
+        desc.innerHTML = project.description;
+        cardBody.append(desc);
+    }
+}
+
+setProjects();
